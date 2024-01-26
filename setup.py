eaaa358c52b641f07ec5dc1bf4e2a75b6f180b67
Chirag Gupta <108172197+chiragchiku@users.noreply.github.com>
2024-01-27 00:08:48 +0530

    fixup setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
   ],
   entry_points={
     'console_scripts': [
-      'topsis-thapar=topsis_chirag_102103278.main:main',
+      'topsis_chirag_102103278=topsis.__main__:main',
     ],
   },
 )
